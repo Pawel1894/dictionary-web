756aa49c68358ae29b1c3823f9178d8e806a8168
Pawel1894 <pawepohl@gmail.com>
2023-01-22 12:04:31 +0100

    Defining fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Lora", "serif"],
+      mono: ["Inconsolata", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
