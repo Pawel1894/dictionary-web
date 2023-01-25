73af64bc668a114d4206f7708ef0feeee9347319
Pawel1894 <pawepohl@gmail.com>
2023-01-25 20:26:56 +0100

    search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div data-testid="appEntry" id="app-entry" data-font="sans serif">
       <Header />
-      <main>app</main>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import SearchIcon from "../../assets/images/icon-search.svg";
+
+export default function Search() {
+  const [searchText, setSearchText] = useState<string>();
+
+  return (
+    <div className="mx-6 relative">
+      <input
+        data-testid="searchInput"
+        className="bg-neutral-700 pl-6 pr-12 py-[0.625rem] border-2 leading-6 border-neutral-700 rounded-2xl text-xl font-bold w-full focus:outline-none focus:border-primary"
+        type="text"
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <img className="absolute top-1/2 -translate-y-1/2 right-5" src={SearchIcon} alt="" aria-hidden={true} />
+    </div>
+  );
+}
