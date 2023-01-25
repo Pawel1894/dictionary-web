c534cf4387af2ab63f558799b1309da9e2124bee
Pawel1894 <pawepohl@gmail.com>
2023-01-25 20:34:59 +0100

    react query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query/build/lib/QueryClientProvider";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div data-testid="appEntry" id="app-entry" data-font="sans serif">
-      <Header />
-      <main>
-        <Search />
-      </main>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div data-testid="appEntry" id="app-entry" data-font="sans serif">
+        <Header />
+        <main>
+          <Search />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
 
