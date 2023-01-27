4c3b486371909766061321533580183f0eff242a
Pawel1894 <pawepohl@gmail.com>
2023-01-27 19:47:00 +0100

    Search input focus on dark mode

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,8 +9,9 @@ export default function Search({ setSearchText }: Props) {
   return (
     <div className="mx-6 relative">
       <input
+        placeholder="Search for any word..."
         data-testid="searchInput"
-        className="bg-neutral-700 pl-6 pr-12 py-[0.625rem] border-2 leading-6 border-neutral-700 rounded-2xl text-base font-bold w-full focus:outline-none focus:border-primary dark:bg-neutral-200 dark:border-neutral-200 dark:text-white"
+        className="bg-neutral-700 pl-6 pr-12 py-[0.625rem] border-2 leading-6 border-neutral-700 rounded-2xl text-base font-bold w-full focus:outline-none focus:border-primary dark:bg-neutral-200 dark:border-neutral-200 dark:focus-within:border-primary dark:text-white"
         type="text"
         onChange={(e) => setSearchText(e.target.value)}
       />
