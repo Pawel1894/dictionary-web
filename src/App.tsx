c084770a030761acaa46b848a135bb20e8dd6f78
Pawel1894 <pawepohl@gmail.com>
2023-01-29 12:44:59 +0100

    searched words cache for 24hr

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Body from "./components/Body";
 import Header from "./components/Header/Header";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+      retry: 0,
+      staleTime: 2000,
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
       <div
         className="max-w-3xl mx-auto mb-20 md:mb-28"
         data-testid="appEntry"
@@ -20,7 +34,7 @@ function App() {
         </main>
         <ReactQueryDevtools initialIsOpen={false} />
       </div>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   );
 }
 
