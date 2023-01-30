781b9d915e707963473b9d272e3c771a5959e6e5
Pawel1894 <pawepohl@gmail.com>
2023-01-30 19:43:15 +0100

    sw

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js");
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-serviceWorkerRegistration.register();
