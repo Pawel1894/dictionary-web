2c092af47d404baa4c93f3e587c32b1ab4fb72fc
Pawel1894 <pawepohl@gmail.com>
2023-01-31 19:24:54 +0100

    Meaning list improvement

--- src/components/Word/Meaning.tsx
+++ src/components/Word/Meaning.tsx
@@ -6,7 +6,7 @@ export default function Meaning({ partOfSpeech, definitions, synonyms }: TMeanin
   return (
     <div className="mb-9 md:mb-11">
       <div className="w-full flex items-center gap-6">
-        <span className="text-neutral-300 w-[61px] font-bold text-lg dark:text-white md:text-2xl">
+        <span className="italic text-neutral-300 w-[61px] font-bold text-lg dark:text-white md:text-2xl">
           {partOfSpeech}
         </span>
         <span className="h-[1px] bg-neutral-600 dark:bg-neutral-400 w-full block"></span>
@@ -15,7 +15,7 @@ export default function Meaning({ partOfSpeech, definitions, synonyms }: TMeanin
         <span className="text-neutral-500 text-base mt-9 mb-4 block md:mt-11 md:text-xl md:mb-7">
           Meaning
         </span>
-        <ul className="list-disc ml-3">
+        <ul className="list-disc ml-3 md:ml-8">
           {definitions
             ? definitions.map((item) => {
                 return (
